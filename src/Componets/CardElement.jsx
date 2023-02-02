5c3ed4e9a627711d09a249840dab345eccb150c6
romulo max <maxromulo16@gmail.com>
2023-02-01 22:09:03 -0300

    Reactor: name icons

--- src/Componets/CardElement.jsx
+++ src/Componets/CardElement.jsx
@@ -8,11 +8,11 @@ function  CardElement(props) {
    <div onClick={() => {props.handleFlip(props.card)}} className= {`card ${props.card.flip ? 'flip' : ''}`} id = {props.card.timeId}>
 
       <div className='front'>
-         <img src={`src/assets/img/icons8-${props.card.nomeTime}-48.png`} alt={props.card.nomeTime} />
+         <img src={`src/assets/${props.card.nomeTime}.png`} alt={props.card.nomeTime} />
       </div>
       
       <div className='back'>
-         <img src=' src/assets/img/icontrofeu.png' alt='Icon Troféu ' />
+         <img src=' src/assets/icontrofeu.png' alt='Icon Troféu ' />
       </div>
    </div>
   );
