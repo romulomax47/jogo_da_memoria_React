17b609b7d9bfee4c10681fea74cfc697dd9fb620
romulo max <maxromulo16@gmail.com>
2023-02-02 18:33:50 -0300

    refactor

--- src/Componets/CardElement.jsx
+++ src/Componets/CardElement.jsx
@@ -8,11 +8,11 @@ function  CardElement(props) {
    <div onClick={() => {props.handleFlip(props.card)}} className= {`card ${props.card.flip ? 'flip' : ''}`} id = {props.card.timeId}>
 
       <div className='front'>
-         <img src={`src/assets/${props.card.nomeTime}.png`} alt={props.card.nomeTime} />
+         <img src={`src/public/assets/${props.card.nomeTime}.png`} alt={props.card.nomeTime} />
       </div>
       
       <div className='back'>
-         <img src=' src/assets/icontrofeu.png' alt='Icon Troféu ' />
+         <img src=' src/public/assets/icontrofeu.png' alt='Icon Troféu ' />
       </div>
    </div>
   );
