434d0d63e6226bd73accf3fdc6b5af5c2d42f6f7
romulo max <maxromulo16@gmail.com>
2023-02-02 21:40:14 -0300

    Build

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MemorieCard from './Componets/App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MemorieCard />
-  </React.StrictMode>,
-)
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base:'/jogo_da_memoria_React',
-  plugins: [react()],
-})
